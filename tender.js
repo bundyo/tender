5eb441146ab8300573bcdee8864dee728eebb9b4
Bundyo <bundyo@gmail.com>
2018-05-30 09:33:25 +0300

    Add initial change watching.

--- tender.js
+++ tender.js
@@ -1,17 +1,54 @@
-const { protocol, session, webContents } = require("electron");
+const { app, protocol, session, webContents } = require("electron");
 const fs = require("fs");
+const path = require("path");
+
+let chokidar;
+try {
+    chokidar = require("chokidar");
+} catch(e) {}
 
 const requests = {};
 
 protocol.registerStandardSchemes(["file"]);
 
-module.exports = {
-    init() {
-        session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
-            requests[details.url] = {
-                id: details.webContentsId
+function updateCSS(filePath, text) {
+    const webContent = webContents.fromId(requests[filePath].id);
+    const selector = `'style[url="${filePath}"]'`;
 
-            };
+    if (webContent && filePath.endsWith(".css")) {
+        webContent.executeJavaScript(`
+            styleTag = document.documentElement.querySelector(${selector});
+            styleElement = document.createElement("style");
+            
+            styleElement.url = \`${filePath}\`;
+            styleElement.textContent = \`${text}\`;
+            
+            if (styleTag) {
+                styleTag.replaceWith(styleElement);
+            } else {
+                document.head.appendChild(styleElement);
+            }
+        `);
+    }
+}
+
+module.exports = {
+    init(initialPath = ".") {
+        session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
+            const filePath = details.url.replace(/^file:\/\//, "");
+            const request = requests[filePath];
+            const id = details.webContentsId;
+
+            if (!request) {
+                requests[filePath] = {
+                    id,
+                    contents: [id]
+                };
+            } else {
+                if (request.contents.indexOf(id) === -1) {
+                    request.contents.push(id);
+                }
+            }
 
             callback({ cancel: false });
         });
@@ -19,7 +56,8 @@ module.exports = {
         protocol.unregisterProtocol("file");
         protocol.registerBufferProtocol("file", (request, callback) => {
             const url = request.url;
-            const buffer = fs.readFileSync(request.url.replace(/^file:\/\//, ""));
+            const filePath = request.url.replace(/^file:\/\//, "");
+            const buffer = fs.readFileSync(filePath);
 
             if (!url.endsWith(".js") && request.headers.Accept === "*/*") {
                 const text = buffer.toString();
@@ -29,30 +67,41 @@ module.exports = {
                     data: Buffer.from(`export default \`${text}\`;`)
                 });
 
-                const webContent = webContents.fromId(requests[url].id);
-                const selector = `'style[url="${url}"]'`;
-
-                if (webContent && url.endsWith(".css")) {
-                    webContent.executeJavaScript(`
-    styleTag = document.documentElement.querySelector(${selector});
-    styleElement = document.createElement("style");
-    
-    styleElement.url = \`${url}\`;
-    styleElement.textContent = \`${text}\`;
-    
-    if (styleTag) {
-        styleTag.replaceWith(styleElement);
-    } else {
-        document.head.appendChild(styleElement);
-    }
-`);
-                }
+                updateCSS(filePath, text);
 
                 return;
             }
 
             callback(buffer);
         });
+
+        let watcher;
+
+        if (chokidar) {
+            watcher = chokidar.watch(initialPath, {
+                ignored: /(^|[\/\\])\..|.*node_modules.*/
+            }).on("ready", () => {
+                watcher.on("all", (event, filePath, stat) => {
+                    filePath = path.resolve(filePath);
+                    const text = fs.readFileSync(filePath).toString();
+
+                    switch (event) {
+                        case "add":
+                            break;
+                        case "change":
+                            updateCSS(filePath, text);
+                            break;
+                        case "unlink":
+                    }
+                });
+            });
+        }
+
+        app.on("quit", () => {
+            if (chokidar) {
+                watcher.close();
+            }
+        })
     }
 };
 
